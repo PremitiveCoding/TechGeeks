a48cd10e52b547778f1272f9de8f9478ac616e54
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2022-09-09 17:44:19 +0200

    geek

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 
   useEffect(() => {
     const getUser = () => {
-      fetch("http://localhost:5000/auth/login/success", {
+      fetch("https://techgeeks.up.railway.app/auth/login/success", {
         method: "GET",
         credentials: "include",
         headers: {
